abb1c8aac8e196a48330f7bd28d94886c385bba5
JophilGulane <jophilgulane.me@gmail.com>
2024-08-30 16:37:24 +0800

    Solved some H4G problems

--- picoCTF/PasswdCrack1/level1.py
+++ picoCTF/PasswdCrack1/level1.py
@@ -1,28 +1,5 @@
-### THIS FUNCTION WILL NOT HELP YOU FIND THE FLAG --LT ########################
-def str_xor(secret, key):
-    #extend key to secret length
-    new_key = key
-    i = 0
-    while len(new_key) < len(secret):
-        new_key = new_key + key[i]
-        i = (i + 1) % len(key)        
-    return "".join([chr(ord(secret_c) ^ ord(new_key_c)) for (secret_c,new_key_c) in zip(secret,new_key)])
-###############################################################################
+import binascii
 
-
-flag_enc = open('level1.flag.txt.enc', 'rb').read()
-
-
-
-def level_1_pw_check():
-    user_pw = input("Please enter correct password for flag: ")
-    if( user_pw == "691d"):
-        print("Welcome back... your flag, user:")
-        decryption = str_xor(flag_enc.decode(), user_pw)
-        print(decryption)
-        return
-    print("That password is incorrect")
-
-
-
-level_1_pw_check()
+hex_string = "qu@l1ty_0f_&eRv!ce\\xmuLt1-f@cTor_AuTh3nt1c@ti0n\\xquErY\\xcL1ckj@ck!ng\\xruLes_0f_enGag3m#nt..."
+decoded_string = binascii.unhexlify(hex_string.replace("\\x", ""))
+print(decoded_string.decode('utf-8'))
